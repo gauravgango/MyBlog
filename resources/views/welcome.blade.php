900d31f6ad1b4243512643bbcc347a9af719abe5
Gaurav Gangopadhyay <gaurav.gango@gmail.com>
2016-06-19 20:28:32 +0530

    add edit by info

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Laravel 5 Editing by Gaurav</div>
             </div>
         </div>
     </body>
